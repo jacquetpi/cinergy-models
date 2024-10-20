6ea79a1cd1651fa0b06eccb1bb255031bde0ebbb
pierre <pierre>
2024-10-20 18:00:03 +0200

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Model generators for cinergy
+
+Measure and calibrate an energy model
+
+## Features
+
+Generate energy models
+
+## Usage
+
+```bash
+python3 cinergy-model.py --help
+```
+
+/!\ RAPL access may require root rights
+
+To dump on default ```consumption.csv``` while also displaying measures to the console
+```bash
+python3 cinergy-model.py --live
+```
+
+To change default values:
+```bash
+python3 cinergy-model.py --delay=(sec) --precision=(number of digits) --output=prefix
+```
---
+++ cinergy-model.py
@@ -0,0 +1,419 @@
+import sys, getopt, re, time
+from os import listdir, kill, killpg, getpgid
+from os.path import isfile, join, exists
+import subprocess, signal
+
+OUTPUT_PREFIX   = 'consumption'
+OUTPUT_HEADER = 'timestamp,domain,measure'
+OUTPUT_NL     = '\n'
+ROOT_FS       ='/sys/class/powercap/'
+PRECISION     = 5
+SYSFS_STAT    = '/proc/stat'
+SYSFS_TOPO    = '/sys/devices/system/cpu/'
+SYSFS_FREQ    = '/sys/devices/system/cpu/{core}/cpufreq/scaling_cur_freq'
+# From https://www.kernel.org/doc/Documentation/filesystems/proc.txt
+SYSFS_STATS_KEYS  = {'cpuid':0, 'user':1, 'nice':2 , 'system':3, 'idle':4, 'iowait':5, 'irq':6, 'softirq':7, 'steal':8, 'guest':9, 'guest_nice':10}
+SYSFS_STATS_IDLE  = ['idle', 'iowait']
+SYSFS_STATS_NTID  = ['user', 'nice', 'system', 'irq', 'softirq', 'steal']
+LIVE_DISPLAY = False
+PER_CACHE_USAGE = None
+VM_CONNECTOR    = None
+
+def print_usage():
+    print('python3 rapl-reader.py [--help] [--live]  [--explicit] [--vm=qemu:///system] [--output=' + OUTPUT_PREFIX + '] [--precision=' + str(PRECISION) + ' (number of decimal)]')
+
+###########################################
+# Find relevant sysfs
+###########################################
+def find_rapl_sysfs():
+    regex = '^intel-rapl:[0-9]+.*$'
+    folders = [f for f in listdir(ROOT_FS) if re.match(regex, f)]
+    # package0: cpu, cores: cores of cpu, uncore : gpu, psys: platform ...
+    sysfs = dict()
+    for folder in folders:
+        base = ROOT_FS + folder
+        with open(base + '/name') as f:
+            domain = f.read().replace('\n','')
+        if '-' not in domain: domain+= '-' + folder.split(':')[1] # We guarantee name unicity
+        sysfs[domain] = base + '/energy_uj'
+    return sysfs
+
+def find_cpuid_per_numa():
+    regex = '^cpu[0-9]+$'
+    cpu_found = [int(re.sub("[^0-9]", '', f)) for f in listdir(SYSFS_TOPO) if not isfile(join('topology', f)) and re.match(regex, f)]
+    cpu_per_numa = dict()
+    for cpu in cpu_found:
+        path = SYSFS_TOPO + 'cpu' + str(cpu) + '/topology/physical_package_id'
+        if not exists(path): continue
+        with open(path, 'r') as f:
+            numa_id = int(f.read())
+        if numa_id not in cpu_per_numa: cpu_per_numa[numa_id] = list()
+        cpu_per_numa[numa_id].append('cpu' + str(cpu))
+    return cpu_per_numa
+
+def find_cache_topo():
+    regex_cpu = '^cpu[0-9]+$'
+    regex_idx = '^index[0-9]+$'
+    cpu_found = [int(re.sub("[^0-9]", '', f)) for f in listdir(SYSFS_TOPO) if re.match(regex_cpu, f)]
+    cpu_per_cache = dict()
+    cpu_found.sort()
+    for cpu in cpu_found:
+        path = SYSFS_TOPO + 'cpu' + str(cpu) + '/cache'
+        cache_found = [int(re.sub("[^0-9]", '', f)) for f in listdir(path) if not isfile(f) and re.match(regex_idx, f)]
+        cache_found.sort()
+        prev_shared = None
+        cache_list_for_cpu = dict()
+        for cache_index in cache_found:
+            path_completed = path + '/' + 'index' + str(cache_index)
+            with open(path_completed + '/shared_cpu_list', 'r') as f:
+                shared = f.read()
+                if shared == prev_shared:
+                    continue
+            with open(path_completed + '/id', 'r') as f:
+                cache_id = int(f.read())
+            cache_list_for_cpu[cache_index] = cache_id
+            prev_shared = shared
+            
+        cache_index_of_interest = list(cache_list_for_cpu.keys())
+        cache_index_of_interest.sort(reverse=True)
+        prev_elem = cpu_per_cache
+
+        for cache_index_unique in cache_index_of_interest:
+            key = 'L' + str(cache_index_unique) + '-' + str(cache_list_for_cpu[cache_index_unique])
+            if cache_index_unique != min(cache_index_of_interest):
+                if key not in prev_elem:
+                    prev_elem[key] = dict()
+                prev_elem = prev_elem[key]
+            else:
+                if key not in prev_elem:
+                    prev_elem[key] = list()
+                prev_elem[key].append(cpu)
+                break
+
+    return cpu_per_cache
+
+###########################################
+# Read CPU usage
+###########################################
+def read_cpu_usage(cpuid_per_numa : dict, hist :dict):
+    measures = dict()
+    global_usage = get_usage_global(cputime_hist=hist)
+    if global_usage != None: measures['cpu%_package-global'] = round(global_usage, PRECISION)
+    for numa_id, cpuid_list in cpuid_per_numa.items():
+        numa_usage = get_usage_of(server_cpu_list=cpuid_list, cputime_hist=hist)
+        numa_freq  = get_freq_of(server_cpu_list=cpuid_list)
+        if numa_usage != None: 
+            measureOUTPUT_PREFIX
+    def clear_time(self):
+        if hasattr(self, 'idle'): delattr(self, 'idle')
+        if hasattr(self, 'not_idle'): delattr(self, 'not_idle')
+
+def get_usage_global(cputime_hist : dict):
+    with open(SYSFS_STAT, 'r') as f:
+        split = f.readlines()[0].split(' ')
+        split.remove('')
+    if 'global' not in cputime_hist: cputime_hist['global'] = CpuTime()
+    return __get_usage_of_line(split=split, hist_object=cputime_hist['global'])
+
+def get_usage_of(server_cpu_list : list, cputime_hist : dict):
+    cumulated_cpu_usage = 0
+    with open(SYSFS_STAT, 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        split = line.split(' ')
+        if not split[SYSFS_STATS_KEYS['cpuid']].startswith('cpu'): break
+        if split[SYSFS_STATS_KEYS['cpuid']] not in server_cpu_list: continue
+
+        if split[SYSFS_STATS_KEYS['cpuid']] not in cputime_hist: cputime_hist[split[SYSFS_STATS_KEYS['cpuid']]] = CpuTime()
+        hist_object = cputime_hist[split[SYSFS_STATS_KEYS['cpuid']]]
+
+        cpu_usage = __get_usage_of_line(split=split, hist_object=hist_object)
+
+        # Add usage to cumulated value
+        if cumulated_cpu_usage != None and cpu_usage != None:
+            cumulated_cpu_usage+=cpu_usage
+        else: cumulated_cpu_usage = None # Do not break to compute others initializing values
+
+    if cumulated_cpu_usage != None: 
+        cumulated_cpu_usage = round(cumulated_cpu_usage/len(server_cpu_list), PRECISION)
+    return cumulated_cpu_usage
+
+def __get_usage_of_line(split : list, hist_object : object, update_history : bool = True):
+    idle          = sum([ int(split[SYSFS_STATS_KEYS[idle_key]])     for idle_key     in SYSFS_STATS_IDLE])
+    not_idle      = sum([ int(split[SYSFS_STATS_KEYS[not_idle_key]]) for not_idle_key in SYSFS_STATS_NTID])
+
+    # Compute delta
+    cpu_usage  = None
+    if hist_object.has_time():
+        prev_idle, prev_not_idle = hist_object.get_time()
+        delta_idle     = idle - prev_idle
+        delta_total    = (idle + not_idle) - (prev_idle + prev_not_idle)
+        if delta_total>0: # Manage overflow
+            cpu_usage = ((delta_total-delta_idle)/delta_total)*100
+    
+    if update_history: hist_object.set_time(idle=idle, not_idle=not_idle)
+    return cpu_usage
+
+def read_core_usage(cputime_hist : dict, update_history : bool):
+    with open(SYSFS_STAT, 'r') as f:
+        lines = f.readlines()
+
+    measures = dict()
+    lines.pop(0) # remove global line, we focus on per cpu usage
+    for line in lines:
+        split = line.split(' ')
+        if not split[SYSFS_STATS_KEYS['cpuid']].startswith('cpu'): break
+
+        if split[SYSFS_STATS_KEYS['cpuid']] not in cputime_hist: cputime_hist[split[SYSFS_STATS_KEYS['cpuid']]] = CpuTime()
+        cpu_usage = __get_usage_of_line(split=split, hist_object=cputime_hist[split[SYSFS_STATS_KEYS['cpuid']]], update_history=update_history)
+        measures['cpu%_' + split[SYSFS_STATS_KEYS['cpuid']]] = cpu_usage
+
+    return measures
+
+def display_cache_usage(cache_topo : dict, cputime_hist : dict):
+    core_usage = read_core_usage(cputime_hist=cputime_hist, update_history=False)
+    associate_usage_to_cache_levels(core_usage=core_usage, cache_topo=cache_topo)
+    print('###')
+
+def associate_usage_to_cache_levels(core_usage : dict, cache_topo, label = None, padding_length = -2):
+    if isinstance(cache_topo, dict):
+        cpu_list = list()
+        for cache_id in cache_topo.keys():
+            
+            child_label = ""
+            if label is not None: child_label = label + '_'
+            child_label += cache_id
+            
+            cpu_list.extend(associate_usage_to_cache_levels(core_usage=core_usage, cache_topo=cache_topo[cache_id], label=child_label, padding_length=padding_length+2))
+
+        if label is not None:
+            values = [core_usage['cpu%_cpu' + str(cpu)] for cpu in cpu_list]
+            if None not in values:
+                usage = sum(values) / len(cpu_list)
+                line = ' ' * padding_length
+                line += label + ' : ' + str(usage)
+                print(line)
+        return cpu_list
+
+    else:
+
+        if label is not None:
+            values = [core_usage['cpu%_cpu' + str(cpu)] for cpu in cache_topo]
+            if None not in values:
+                usage = sum(values) / len(cache_topo)
+                line = ' ' * padding_length
+                line += label + ' : ' + str(usage)
+                if usage>51: print(line)
+        return cache_topo
+
+def get_freq_of(server_cpu_list : list):
+    cumulated_cpu_freq = 0
+    for cpu in server_cpu_list:
+        with open(SYSFS_FREQ.replace('{core}', str(cpu)), 'r') as f:
+            cumulated_cpu_freq+= int(f.read())
+    return round(cumulated_cpu_freq/len(server_cpu_list), PRECISION)
+
+###########################################
+# Read libvirt
+###########################################
+
+def read_libvirt():
+    count = 0
+    cpu_cumul = 0
+    mem_cumul = 0
+    for domain_id in VM_CONNECTOR.listDomainsID():
+        try:
+            virDomain = VM_CONNECTOR.lookupByID(domain_id)
+            cpu_cumul+=virDomain.maxVcpus()
+            mem_cumul+=int(virDomain.maxMemory()/1024)
+            count+=1
+        except libvirt.libvirtError as ex:  # VM is not alived anymore
+            pass
+    return {'libvirt_vm_count': count, 'libvirt_vm_cpu_cml': cpu_cumul, 'libvirt_vm_mem_cml': mem_cumul}
+
+###########################################
+# Read joule file, convert to watt
+###########################################
+def read_rapl(rapl_sysfs : dict, hist : dict, current_time : int):
+    measures = dict()
+    overflow = False
+    package_global_joule = 0
+    package_global_watt = 0
+    for domain, file in rapl_sysfs.items():
+        joule, watt = read_joule_file(domain=domain, file=file, hist=hist, current_time=current_time)
+        if watt !=None:
+            measures[domain + '-joule'] = round(joule,PRECISION)
+            measures[domain + '-watt'] = round(watt,PRECISION)
+            if 'package-' in domain: 
+                package_global_joule += joule
+                package_global_watt  += watt
+        else: overflow=True
+
+    # Track time for next round
+    hist['time'] = current_time
+
+    if measures:
+        if not overflow: 
+            measures['package-global-joule'] = round(package_global_joule,PRECISION)
+            measures['package-global-watt']  = round(package_global_watt,PRECISION)
+    return measures
+
+def read_joule_file(domain : str, file : str, hist : dict, current_time : int):
+    # Read file
+    with open(file, 'r') as f: current_uj_count = int(f.read())
+
+    # Compute delta
+    current_uj_delta = current_uj_count - hist[domain] if hist[domain] != None else None
+    hist[domain] = current_uj_count # Manage hist for next delta
+
+    # Manage exceptional cases
+    if current_uj_delta == None: return None # First call
+    if current_uj_delta < 0: return None # Overflow
+
+    # Convert to watt
+    current_us_delta = (current_time - hist['time'])/1000 #delta with ns to us
+    current_joule = current_uj_delta / (10**6) # Convert microjoules to joules (1 J = 1,000,000 µJ)
+    current_watt = current_uj_delta/current_us_delta
+
+    return current_joule, current_watt
+
+###########################################
+# Main functions
+###########################################
+def gen_model(rapl_sysfs : dict, cpuid_per_numa : dict, cache_topo : dict):
+    step  = 25
+    delay = 1
+    size = 0
+    for cpuid in cpuid_per_numa.values(): size+=len(cpuid)
+    progress = 0
+    # Capture idle
+    print('gen_model target', progress, '%')
+    # Capture workload
+    for numa in cpuid_per_numa.keys():
+        for cpuid in cpuid_per_numa[numa]:
+            print(cpu_list, range(int(100/step)))
+            for _ in range(int(100/step)):
+                progress+=1
+                progress_percentage = int(round((progress/(size/(step/100))),2)*100)
+                print('gen_model target', progress_percentage, '%')
+                process_to_kill.append(subprocess.Popen("stress-ng -c 1 -l " + str(step), shell=True))
+                time.sleep(delay)
+
+    for process in process_to_kill: killpg(getpgid(process.pid), signal.SIGTERM)
+
+def gen_exp(rapl_sysfs : dict, cpuid_per_numa : dict, cache_topo : dict):
+    # Launch VM and capture (groundthruth)
+    process_to_kill.append(subprocess.Popen("./test.sh", shell=True))
+    time.sleep(5)
+    for process in process_to_kill: killpg(getpgid(process.pid), signal.SIGTERM)
+    # Redo with noise
+    process_to_kill.append(subprocess.Popen("./test.sh", shell=True))
+    time.sleep(5)
+    for process in process_to_kill: killpg(getpgid(process.pid), signal.SIGTERM)
+
+def read_and_sleep(label : str, rapl_sysfs : dict, cpuid_per_numa : dict, cache_topo : dict, misc : dict = {}, sleep : int = 0, init : bool = False):
+    if init:
+        rapl_hist = {name:None for name in rapl_sysfs.keys()}
+        rapl_hist['time'] = None # for joule to watt conversion
+        cpu_hist = dict()
+        launch_at = time.time_ns()
+        output_file = OUTPUT_PREFIX + '-' + label + '.csv'
+        with open(output_file, 'w') as f: f.write(OUTPUT_HEADER + OUTPUT_NL)
+        time_begin = time.time_ns()
+
+        rapl_measures = read_rapl(rapl_sysfs=rapl_sysfs, hist=rapl_hist, current_time=time_begin)
+        cpu_measures  = dict()
+        if PER_CACHE_USAGE:
+            display_cache_usage(cputime_hist=cpu_hist, cache_topo=cache_topo)
+        for key, value in read_cpu_usage(cpuid_per_numa=cpuid_per_numa, hist=cpu_hist).items(): cpu_measures[key] = value
+
+        libvirt_measures = dict()
+        if VM_CONNECTOR != None: libvirt_measures = read_libvirt()
+
+        output(output_file=output_file, rapl_measures=rapl_measures, cpu_measures=cpu_measures, libvirt_measures=libvirt_measures, misc=misc, time_since_launch=int((time_begin-launch_at)/(10**9)))
+
+        time_to_sleep = (sleep*10**9) - (time.time_ns() - time_begin)
+        if time_to_sleep>0: time.sleep(time_to_sleep/10**9)
+        else: print('Warning: overlap iteration', -(time_to_sleep/10**9), 's')
+
+def output(output_file : str, rapl_measures : dict, cpu_measures : dict, libvirt_measures : dict, misc : dict, time_since_launch : int):
+
+    if LIVE_DISPLAY and rapl_measures:
+        max_domain_length = len(max(list(rapl_measures.keys()), key=len))
+        max_measure_length = len(max([str(value) for value in rapl_measures.values()], key=len))
+        for domain, measure in rapl_measures.items():
+            usage_complement = ''
+            for package, cpu_usage in cpu_measures.items():
+                if domain in package:
+                    usage_complement+= '- ' + str(cpu_usage) + '%'
+                    break
+            print(domain.ljust(max_domain_length), str(measure).ljust(max_measure_length), 'W', usage_complement)
+        if libvirt_measures: print('Libvirt:', libvirt_measures['libvirt_vm_count'], 'vm(s)', libvirt_measures['libvirt_vm_cpu_cml'], 'cpu(s)', libvirt_measures['libvirt_vm_mem_cml'], 'MB')
+        print('---')
+
+    # Dump reading
+    with open(output_file, 'a') as f:
+        for domain, measure in misc.items():
+            f.write(str(time_since_launch) + ',' + domain + ',' + str(measure) + OUTPUT_NL)
+        for domain, measure in rapl_measures.items():
+            f.write(str(time_since_launch) + ',' + domain + ',' + str(measure) + OUTPUT_NL)
+        for cpuid, measure in cpu_measures.items():
+            f.write(str(time_since_launch) + ',' + cpuid + ',' + str(measure) + OUTPUT_NL)
+        for metric, value in libvirt_measures.items():
+            f.write(str(time_since_launch) + ',' + metric + ',' + str(value) + OUTPUT_NL)
+
+###########################################
+# Entrypoint, manage arguments
+###########################################
+if __name__ == '__main__':
+
+    short_options = 'hlecdv:o:p:'
+    long_options = ['help', 'live', 'explicit', 'cache', 'vm=', 'delay=', 'output=', 'precision=']
+
+    try:
+        arguments, values = getopt.getopt(sys.argv[1:], short_options, long_options)
+    except getopt.error as err:
+        print(str(err))
+        print_usage()
+    for current_argument, current_value in arguments:
+        if current_argument in ('-h', '--help'):
+            print_usage()
+            sys.exit(0)
+        elif current_argument in('-l', '--live'):
+            LIVE_DISPLAY= True
+        elif current_argument in('-e', '--explicit'):
+            EXPLICIT_USAGE = True
+        elif current_argument in('-c', '--cache'):
+            PER_CACHE_USAGE = True
+        elif current_argument in('-v', '--vm'):
+            import libvirt 
+            VM_CONNECTOR = libvirt.open(current_value)
+            if not VM_CONNECTOR: raise SystemExit('Failed to open connection to ' + current_value)
+        elif current_argument in('-o', '--output'):
+            OUTPUT_PREFIX= current_value
+        elif current_argument in('-p', '--precision'):
+            PRECISION= int(current_value)
+
+    try:
+        # Find sysfs
+        rapl_sysfs=find_rapl_sysfs()
+        cpuid_per_numa=find_cpuid_per_numa()
+        cache_topo=find_cache_topo()
+        process_to_kill=list()
+        print('>RAPL domain found:')
+        max_domain_length = len(max(list(rapl_sysfs.keys()), key=len))
+        for domain, location in rapl_sysfs.items(): print(domain.ljust(max_domain_length), location)
+        print('>NUMA topology found:')
+        for numa_id, cpu_list in cpuid_per_numa.items(): print('socket-' + str(numa_id) + ':', len(cpu_list), 'cores')
+        print('')
+
+        #gen_model(rapl_sysfs=rapl_sysfs, cpuid_per_numa=cpuid_per_numa, cache_topo=cache_topo)
+        gen_exp(rapl_sysfs=rapl_sysfs, cpuid_per_numa=cpuid_per_numa, cache_topo=cache_topo)
+
+        # Launch
+        #loop_read(rapl_sysfs=rapl_sysfs, cpuid_per_numa=cpuid_per_numa, cache_topo=cache_topo)
+    except KeyboardInterrupt:
+        for process in process_to_kill: killpg(getpgid(process.pid), signal.SIGTERM)
+        print('Program interrupted')
+        sys.exit(0)
